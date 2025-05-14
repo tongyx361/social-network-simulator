0bfee8fb790c4541538d4597ffce9181df350805
Shawn/Yuxuan Tong <tongyuxuan361@gmail.com>
2025-05-14 23:14:31 +0800

    fix: bug from oasis

--- examples/demo.py
+++ examples/demo.py
@@ -1,15 +1,22 @@
+import asyncio
 import logging
 import os
+from pathlib import Path
 
+import nest_asyncio
 import pandas as pd
 import streamlit as st
 from oasis.social_platform.typing import ActionType
+from stqdm import stqdm
+
+# c.f. https://sehmi-conscious.medium.com/got-that-asyncio-feeling-f1a7c37cab8b
+nest_asyncio.apply()
 
 logger = logging.getLogger(__name__)
 
 
 if os.environ.get("DEBUG", "false").lower() == "true":
-    logger.setLevel(logging.DEBUG)
+    logging.basicConfig(level=logging.DEBUG)
     import importlib
 
     from sns import simulation
@@ -18,6 +25,8 @@ if os.environ.get("DEBUG", "false").lower() == "true":
     importlib.reload(simulation)
     importlib.reload(temporal_graph)
 
+
+from sns.simulation import SimulationConfig, simulate_twitter  # noqa: E402
 
 AGENT_INFO_FIELDS = [
     "user_id",
@@ -29,47 +38,107 @@ AGENT_INFO_FIELDS = [
     "description",
 ]
 
+
+DEFAULT_AVAILABLE_ACTIONS = [
+    ActionType.DO_NOTHING.value,
+    ActionType.REPOST.value,
+    ActionType.LIKE_POST.value,
+    ActionType.FOLLOW.value,
+]
+
+STATE2DEFAULT = {
+    "simu_configs": None,
+    "edited_base_agent_info_df": None,
+    "edited_exp_agent_info_df": None,
+}
+
+# NOTE: Setting `key` also sets `st.session_state[key]`
 if __name__ == "__main__":
+    for state_key, default_value in STATE2DEFAULT.items():
+        if state_key not in st.session_state:
+            st.session_state[state_key] = default_value
+
     st.set_page_config(page_title="Social Network Simulator", layout="wide")
     st.title("Social Network Simulator")
 
     st.sidebar.header("Simulation Settings")
-    # Use session state to keep track of num_timesteps if needed across runs/interactions
-    if "num_timesteps" not in st.session_state:
-        st.session_state.num_timesteps = 3  # Default value
 
-    # Update session state when number input changes
-    st.session_state.num_timesteps = st.sidebar.number_input(
-        "Number of Timesteps", min_value=1, value=st.session_state.num_timesteps, step=1, key="num_timesteps_input"
-    )
-    num_timesteps = st.session_state.num_timesteps  # Get value for local use
-
-    available_actions = st.sidebar.multiselect(
+    st.sidebar.multiselect(
         "Available Actions",
         options=[action.value for action in ActionType],
-        default=[
-            ActionType.DO_NOTHING.value,
-            ActionType.REPOST.value,
-            ActionType.LIKE_POST.value,
-            ActionType.FOLLOW.value,
-        ],
-        key="available_actions_select",
+        default=DEFAULT_AVAILABLE_ACTIONS,
+        key="available_actions",
     )
 
+    # The uploaded `base_agent_info_file` is immediately used to build the `base_agent_info_df`
+    # without the need to put it in `st.session_state`
     base_agent_info_file = st.sidebar.file_uploader("Upload Base Agent Information", type="csv")
-    base_agent_info_df = (
-        pd.read_csv(base_agent_info_file, index_col=0)
-        if base_agent_info_file is not None
-        else pd.DataFrame(columns=AGENT_INFO_FIELDS)
+    just_read_df = pd.read_csv(base_agent_info_file, index_col=0) if base_agent_info_file is not None else None
+    # logger.debug(f"{just_read_df=}")
+    base_agent_info_df = just_read_df if just_read_df is not None else pd.DataFrame(columns=AGENT_INFO_FIELDS)
+    assert set(base_agent_info_df.columns) == set(AGENT_INFO_FIELDS), (
+        f"{set(base_agent_info_df.columns)=} != {set(AGENT_INFO_FIELDS)=}"
     )
-    logger.debug(f"{base_agent_info_df=}")
-    assert set(base_agent_info_df.columns) == set(AGENT_INFO_FIELDS)
-    st.sidebar.write("Base Agent Information:")
-    edited_base_agent_info_df = st.sidebar.data_editor(base_agent_info_df, key="base_agent_info", num_rows="dynamic")
 
-    st.write("Agent Information for Comparison:")
-    edited_agent_info = st.data_editor(
-        edited_base_agent_info_df,  # Following the change of edited_base_agent_info_df
-        key="edited_agent_info",
-        num_rows="dynamic",
+    st.sidebar.write("Base Agent Information")
+    # `data_editor(key="data_editor")` only saves the changes to `st.session_state["data_editor"]`
+    st.session_state["edited_base_agent_info_df"] = st.sidebar.data_editor(
+        base_agent_info_df, num_rows="dynamic", key="edited_base_agent_info_df_changes"
     )
+    logger.debug(f"{st.session_state['edited_base_agent_info_df']=}")
+
+    # Main
+    st.write("Agent Information for Comparison (Refreshing if base changes)")
+    st.session_state["edited_exp_agent_info_df"] = st.data_editor(
+        st.session_state["edited_base_agent_info_df"], num_rows="dynamic", key="edited_exp_agent_info_df_changes"
+    )
+
+    base_check_col, exp_check_col = st.columns(2)
+    with base_check_col:
+        st.checkbox("Base", value=True, key="simulate_base")
+    with exp_check_col:
+        st.checkbox("Experiment", value=True, key="simulate_exp")
+
+    # Prepare simulation configurations based on UI selections
+
+    st.number_input("Run for Timesteps", min_value=1, value=3, step=1, key="num_timesteps")
+    st.checkbox("Sequential", value=True, key="sequential")
+    if st.button("Run Selected Simulations"):
+        simu_db_home = Path("./data/simu_db")
+        viz_home = Path("./data/visualization")
+
+        simu_configs = []
+        if st.session_state["simulate_base"]:
+            simu_configs.append(
+                SimulationConfig(
+                    agent_info=st.session_state["edited_base_agent_info_df"],
+                    available_actions=st.session_state["available_actions"],
+                    db_path=simu_db_home / "base.db",
+                    visualization_home=viz_home / "base",
+                    num_timesteps=st.session_state["num_timesteps"],
+                )
+            )
+        if st.session_state["simulate_exp"]:
+            simu_configs.append(
+                SimulationConfig(
+                    agent_info=st.session_state["edited_exp_agent_info_df"],
+                    available_actions=st.session_state["available_actions"],
+                    db_path=simu_db_home / "exp.db",
+                    visualization_home=viz_home / "exp",
+                    num_timesteps=st.session_state["num_timesteps"],
+                )
+            )
+
+        loop = asyncio.get_event_loop()
+        try:
+            if st.session_state["sequential"]:
+                for config in stqdm(simu_configs, desc="Running Simulations"):
+                    # logger.debug(f"Running simulation with {config=}")
+                    loop.run_until_complete(simulate_twitter(config))
+            else:
+                with st.spinner("Running Simulations..."):
+                    loop.run_until_complete(asyncio.gather(*[simulate_twitter(config) for config in simu_configs]))
+        except Exception as e:
+            st.error(f"Error running simulations: {e}")
+            raise e
+        st.success("Simulations completed successfully")
--- src/sns/simulation.py
+++ src/sns/simulation.py
@@ -1,6 +1,7 @@
 import asyncio
 import logging
 import os
+from dataclasses import dataclass, field
 from pathlib import Path
 
 import pandas as pd
@@ -11,61 +12,84 @@ from oasis.clock.clock import Clock
 from oasis.social_agent.agents_generator import generate_agents
 from oasis.social_platform.channel import Channel
 from oasis.social_platform.platform import Platform
-from oasis.social_platform.typing import ActionType
+from oasis.social_platform.typing import ActionType, RecsysType
 
 logger = logging.getLogger(__name__)
 
 
-async def simulate_twitter(
-    available_actions: list[ActionType],
-    agent_info: pd.DataFrame,
-    visualization_home: Path | None = None,
-    db_path: str = ":memory:",
-    num_timesteps: int = 3,
-    clock_factor: int = 60,
-    recsys_type: str = "twhin-bert",
-) -> None:
-    if os.path.exists(db_path):
-        os.remove(db_path)
-    Path(db_path).parent.mkdir(parents=True, exist_ok=True)
+@dataclass
+class ModelConfig:
+    model_platform: ModelPlatformType = ModelPlatformType.ZHIPU
+    model_type: ModelType = ModelType.GLM_4_FLASH
+
+
+@dataclass
+class SimulationConfig:
+    agent_info: pd.DataFrame
+    db_path: Path
+    model_config: ModelConfig = field(default_factory=ModelConfig)
+    available_actions: list[ActionType] = field(
+        default_factory=lambda: [
+            ActionType.DO_NOTHING.value,
+            ActionType.REPOST.value,
+            ActionType.LIKE_POST.value,
+            ActionType.FOLLOW.value,
+        ]
+    )
+    visualization_home: Path | None = None
+    num_timesteps: int = 3
+    clock_factor: int = 60
+    recsys_type: RecsysType = RecsysType.TWHIN
+
+
+async def simulate_twitter(config: SimulationConfig) -> None:
+    # print(f"Simulating Twitter with {config=}")
+    if os.path.exists(config.db_path):
+        os.remove(config.db_path)
+    Path(config.db_path).parent.mkdir(parents=True, exist_ok=True)
+    if config.visualization_home is not None:
+        Path(config.visualization_home).mkdir(parents=True, exist_ok=True)
 
     start_time = 0
-    clock = Clock(k=clock_factor)
+    clock = Clock(k=config.clock_factor)
     twitter_channel = Channel()
     infra = Platform(
-        db_path,
-        twitter_channel,
-        clock,
-        start_time,
-        recsys_type=recsys_type,
+        db_path=config.db_path,
+        channel=twitter_channel,
+        sandbox_clock=clock,
+        recsys_type=config.recsys_type,
+        start_time=start_time,
         refresh_rec_post_count=2,
         max_rec_post_len=2,
         following_post_count=3,
     )
     twitter_task = asyncio.create_task(infra.running())
+    model_config = config.model_config
     models = [
         ModelFactory.create(
-            model_platform=ModelPlatformType.ZHIPU,
-            model_type=ModelType.GLM_4_FLASH,
+            model_platform=model_config.model_platform,
+            model_type=model_config.model_type,
         )
     ]
 
+    # print(f"Generating agent graph with {config.agent_info=}")
     agent_graph = await generate_agents(
-        agent_info=agent_info,
+        agent_info=config.agent_info,
         twitter_channel=twitter_channel,
         start_time=start_time,
         model=models,
-        recsys_type=recsys_type,
-        available_actions=available_actions,
+        recsys_type=config.recsys_type,
+        available_actions=config.available_actions,
         twitter=infra,
     )
-    if visualization_home is not None:
-        agent_graph.visualize(visualization_home / "initial_social_graph.png")
+    if config.visualization_home is not None:
+        agent_graph.visualize(config.visualization_home / "initial_social_graph.png")
 
-    for timestep in range(1, num_timesteps + 1):
+    # print(f"Running simulation for {config.num_timesteps=}")
+    for timestep in range(1, config.num_timesteps + 1):
         clock.time_step = timestep * 3
         logger.info(f"timestep:{timestep}")
-        db_file = db_path.split("/")[-1]
+        db_file = config.db_path.name
         print(Back.GREEN + f"DB:{db_file} timestep:{timestep}" + Back.RESET)
         # if you want to disable recsys, please comment this line
         await infra.update_rec_table()
@@ -75,8 +99,21 @@ async def simulate_twitter(
             tasks.append(agent.perform_action_by_llm())
 
         await asyncio.gather(*tasks)
-        if visualization_home is not None:
-            agent_graph.visualize(visualization_home / f"timestep_{timestep}_social_graph.png")
+        if config.visualization_home is not None:
+            agent_graph.visualize(config.visualization_home / f"timestep_{timestep}_social_graph.png")
 
+    # print("Waiting for simulation to finish...")
     await twitter_channel.write_to_receive_queue((None, None, ActionType.EXIT))
     await twitter_task
+
+
+if __name__ == "__main__":
+    asyncio.run(
+        simulate_twitter(
+            SimulationConfig(
+                agent_info=pd.read_csv("./data/agent_info/False_Business_0.csv"),
+                db_path=Path("./data/simu_db/test.db"),
+                visualization_home=Path("./data/visualization/test"),
+            )
+        )
+    )
